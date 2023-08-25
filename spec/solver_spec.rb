ff82100e0795375731bfa307fc7f10f5b900e0e4
beteland123 <beteland23@gmail.com>
2023-08-25 18:59:17 +0300

    add test for the factorial postive number

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,3 +1,9 @@
 require_relative '../solver'
 describe Solver do
+    describe "#factorial" do
+    it "returns 24 for the factorial of the number 4" do
+        solver = Solver.new
+        expect(solver.factorial(4)).to eql(24)
+    end
+end
 end
