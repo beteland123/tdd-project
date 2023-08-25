f8b25858441179a04dc69434ef6e6798a91c0053
beteland123 <beteland23@gmail.com>
2023-08-25 19:43:51 +0300

    fix the failed test for rasing exception

--- solver.rb
+++ solver.rb
@@ -1,10 +1,12 @@
 class Solver
     def factorial(num)
-        if num == 0 || num == 1
+        if num < 0
+           return raise ArgumentError, "Factorial is not defined for negative numbers."
+        elsif num == 0 || num == 1
             return 1
-          else
+        else
             return num * factorial(num - 1)
-          end
+        end
     end
 
 end
