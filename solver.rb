49746999ebd5999221f166b5d342e2a497436331
beteland123 <beteland23@gmail.com>
2023-08-25 19:23:31 +0300

    fix the failed test by adding the functionlaity for the factorial

--- solver.rb
+++ solver.rb
@@ -1,6 +1,10 @@
 class Solver
     def factorial(num)
-       return 24
+        if num == 0 || num == 1
+            return 1
+          else
+            return num * factorial(num - 1)
+          end
     end
 
 end
