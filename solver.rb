adfd04bc95e876483a77d3faa8af10e628f5a4bc
beteland123 <beteland23@gmail.com>
2023-08-25 20:20:05 +0300

    fix test for divisible by 3 from expecting nil to fizz

--- solver.rb
+++ solver.rb
@@ -18,6 +18,8 @@ class Solver
     def fizzbuzz(num)
         if num % 3 ==0 && num % 5 == 0
             return "fizzbuzz"
+        elsif num % 3 ==0 
+             return "fizz"
         end
     end
 
